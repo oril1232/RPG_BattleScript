4c9ce931f05df40310eb2e80d0d6e31fef0792d5
Ori Lavi <oril1232@gmail.com>
2023-08-17 19:46:31 +0300

    we added getters and other utility stuff and we fixed the magic book

--- classes/game.py
+++ classes/game.py
@@ -11,16 +11,58 @@ class bcolors:
     UNDERLINE='\033[4m'
 
 class Player:
-    def __init__(self, HP, MP, ATK, DEF, Magic):
-        self.MaxHP = HP
-        self.CurrentHP = HP
-        self.MaxMP = MP
-        self.CurrentMP = MP
-        self.ATK_Low = ATK-10
-        self.ATK_High = ATK + 10
-        self.DEF = DEF
-        self.Magic = Magic
+    def __init__(self, hp, mp, atk, defence, magic):
+        self.max_hp = hp
+        self.currenthp = hp
+        self.maxmp = mp
+        self.currentmp = mp
+        self.atk_low = atk - 10
+        self.atk_high = atk + 10
+        self.defence = defence
+        self.magic = magic
         self.actions=["Attack","Magic"]
 
-    def ATK_Damge(self):
-        return  random.randrange(self.ATK_High,self.ATK_Low)
+    def atk_damge(self):
+        return  random.randrange(self.atk_high, self.atk_low)
+    def magic_damage(self,i):
+        lmagic=self.magic[i]["Damage"]-5
+        hmagic=self.magic[i]["Damage"]+5
+        return random.randrange(lmagic, hmagic)
+    def self_dmg (self,dmg):
+        HP= self.currenthp - dmg
+        if HP<0:
+            HP=0
+        return HP
+    def get_max_hp(self):
+        return self.MaxHP
+
+    def get_current_hp(self):
+        return self.currenthp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def get_current_mp(self):
+        return self.currentmp
+    def reduce_mp(self,cost):
+        self.currentmp-=cost
+
+    def get_spell_name(self,i):
+        return self.magic[i]["Name"]
+    def get_spell_mp_cost(self,i):
+        return self.magic[i]["Cost"]
+
+    def choose_action(self):
+        i=1
+        print("Actions:")
+        for item in self.actions:
+            print(str(i)+":",item)
+            i+=1
+
+
+    def choose_magic(self):
+        i=1
+        print("Magic Spells")
+        for spell in self.magic:
+            print(str(i)+":",spell["Name"],"(cost:",str(spell["Cost"],")"))
+            i+=1
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from classes.game import Player,bcolors
 import random
 
-magic=[{"Name":"Fireball","Cost":"25","Damage":random.randrange(8,48)},
-       {"Name":" Lightning Bolt ","Cost":"25","Damage":random.randrange(8,48)},
-       {"Name":"Ray Of Frost","Cost":"5","Damage":random.randrange(1,8)}]
-player=Player(460,100,30,30,magic)
+magic=[{"Name":"Fireball","Cost":25,"Damage":28},
+       {"Name":" Lightning Bolt ","Cost":25,"Damage":28},
+       {"Name":"Ray Of Frost","Cost":5,"Damage":4}]
+player= Player(460, 100, 30, 30, magic)
+
